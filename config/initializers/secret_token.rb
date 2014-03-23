45294ad065c81d8b9dd4bb89ac24df158ecbf122
Johnathon <ja.lueth@gmail.com>
2014-03-23 14:50:41 -0500

    Initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectWorkhang::Application.config.secret_token = 'bc487f5c95d413bddaa3b40d834a1219fd87d0a9003112fbee90ae352a673c6f7e16973b5b2483d6474ba81a0593624ceba5f304687612115a9e0cbf5cfcc43d'
