d97ed53d822bd9ea04e055ccd8b7d584d2554b8e
Michael Todd <mikejtodd@gmail.com>
2014-03-24 15:07:52 -0500

    Add groups controller and view files

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ ProjectWorkhang::Application.routes.draw do
   get "/checkin/:id" => 'pages#checkin', :as => :checkin
   get "/about" => 'pages#about', :as => :about
   get "/checkout/:id" => 'pages#checkout', :as => :checkout
-  get "/logout" => 'pages#logout', as => :logout
+  get "/logout" => 'pages#logout', :as => :logout
   post "/logins" => 'logins#create'
   delete "/logins/:id" => 'logins#destroy'
   resources :users
