2f9ab5c8370993e0b082effa879ab73a883e00e8
Yofred Moik <ytmoik@gmail.com>
2014-03-25 19:08:26 -0400

    fixed full_name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
   
   attr_accessible :email, :fname, :lname, :password, :password_confirmation, :phone_num, :twitter, :website, :projects, :mentor, :bio
-
-  
-  # before do
-  #   def full_name(fname, lname)
-  #     @full_name = "#{fname.titleize} #{lname.titleize}"
-  #   end
-  # end
-
-  def normalize_name
-    self.fname = self.fname.downcase.titleize
-    self.lname = self.lname.downcase.titleize
-  end
   
   def full_name
-    "#{self.fname} #{self.lname}"
+    "#{self.fname.downcase.titleize} #{self.lname.downcase.titleize}"
   end
 
 end
