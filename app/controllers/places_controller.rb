00514de43dff058b92ba12c915a7bda193141be9
Johnathon <ja.lueth@gmail.com>
2014-03-26 19:41:45 -0500

    review frame

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -52,46 +52,6 @@ class PlacesController < ApplicationController
     end
   end
   
-  
-  #def create    
-    #User submitted a query to find a place:
-    #@place = Place.new(params[:place])
-    #Assign search query submitted by user, format to remove uncooperative characters
-    #search_query=(@place.name).downcase.strip.gsub(' ','+')
-    #Send search query to google to find the location(Validate Location)
-    #val_loc=JSON.load(open("https://maps.googleapis.com/maps/api/place/textsearch/json?key=#{ENV["GOOGLE_API_KEY"]}&location=41.2918589,-96.0812485&radius=5000&query=#{search_query}&sensor=false"))
-    #Find lat/long from hash returned on the JSON request, assign to variables
-    # query_google_by_name
- #    if query_google_by_name
- #      latitude=val_loc["results"][0]["geometry"]["location"]["lat"]
- #      longitude=val_loc["results"][0]["geometry"]["location"]["lng"]
- #    else 
- #      redirect_to("/places/new_manual/#{@place.name}")
- #    end
- #    #Query server to find out if the location is in the database already,
- #    @check_db=Place.where({:lat=>latitude, :lng=>longitude}).first
- #    #If place is in db go to that page
- #    if @check_db
- #      redirect_to("/places/#{@check_db.id}")
- #      #Save the new location to the database using the information from google api as a helper.
- #      #Need to be able to search by name, but if it is a franchise run through results to see which one is in the DB and select another.
- #    else
- #      @place.name = val_loc["results"][0]["name"]
- #      format_address=val_loc["results"][0]["formatted_address"]
- #      @place.street = format_address
- #      @place.lng=longitude
- #      @place.lat=latitude
- #      if val_loc["results"][0]["photos"]!=nil
- #          @place.photoref=val_loc["results"][0]["photos"][0]["photo_reference"]
- #      else 
- #        @place.photoref=""
- #      end
- #      @place.save
- #      
- #      redirect_to("/places/#{@place.id}")
- #    end
- #  end
- #  
    def new_manual
      #Google could not appease end user with results
      @place=Place.new
@@ -141,22 +101,19 @@ class PlacesController < ApplicationController
     @imgloc="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{@place.photoref}&sensor=false&key=#{ENV['GOOGLE_API_KEY']}"
     @user = current_user
     @check_ins = current_user.check_ins.where(time_out: nil && place_id == @place.id)
+    @reviews=Review.where(:place_id => @place.id)
   end
   
-  #Return correctly formatted string of coordinates for locations in db by passing in id
-  def deliver_coordinates(id)
-    @created_place=Place.find_by_id(id)
-    return coordinates="#{@created_place.lng},#{@created_place.lat}"
+  def new_review
+    @review=Review.new
   end
-  #Return name of location by passing in id
-  def deliver_name(id)
-    @created_place=Place.find_by_id(id)
-    @created_place.name
+  
+  def new_review_save
+    @place=Place.find_by_id(params[:place_id])
+    @user=current_user
+    @review.user_id=@user.id
+    @review.place_id=@place.ids
+    @review.save
   end
-  #Return an array of objects where users are checked in currently, will run an each on to create js markers for google map(helper?)
-  def deliver_checked_in
-    @checked_in=Place.where(:occupied => true)
-    return @checked_in
-  end
-
+  
 end
