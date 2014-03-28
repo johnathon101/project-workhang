3f78ca93741b10ba500e07ce09d0bb84ff8bf293
Michael Todd <mikejtodd@gmail.com>
2014-03-28 15:24:11 -0500

    Fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,24 +17,25 @@ class PagesController < ApplicationController
     end
     @check_ins = @new_check
     @places = []
-    @user_ids = []
+    @users = []
     @times = []
     @check_ins.each do |check_in|
       place = check_in.place_id
       user = check_in.user_id
       time = check_in.created_at
       @places << place
-      @user_ids << user
+      @users << user
       @times << time
     end
     end
-    
-    @users = User.where(:id => @user_ids)
+
+    @users = User.where(:id => @users)
     @places = Place.where(:id => @places)
     @user_gravs=Array.new
     @users.each do |user|
-      location=CheckIn.where(:user_id => user.id, :time_out => nil).first
-      @user_gravs <<[Gravatar.new(user.email).image_url + "?s=75", location.place_id]
+      location=CheckIn.where(:user_id => user.id, :time_out => nil).each do |place|
+      @user_gravs <<[Gravatar.new(user.email).image_url + "?s=75", place.place_id]
+      end
     end
     gon.places = @places
     gon.group_people=@user_gravs
