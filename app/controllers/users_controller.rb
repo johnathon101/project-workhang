fe955a77e7f8841edf9d47d3fd34176b67aa096d
Yofred Moik <ytmoik@gmail.com>
2014-03-28 00:58:17 -0400

    top 5 / most recent places half-working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,44 +2,22 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
-    
     @users.each do |user|
       @avatar = Gravatar.new(user.email).image_url + "?s=75"
     end
-    
   end
 
   def show
     @user = User.find(params[:id])
     @avatar = Gravatar.new(@user.email).image_url + "?s=200"
     
+    @recent_places = Place.where(:id => CheckIn.where(:user_id => @user.id)).order("created_at DESC").first(3)
     
-    # @check_ins = User.check_ins.first(5)
-    # @fav_places = @user.check_ins.place_id.order
-    
-    # @fav_places = []
-    # @fav_places << CheckIn.where(:user_id => @user.id) && CheckIn.place_id.size.most(5)
-    
-    # @recent_places = []
-    # @recent = Place.where(:).order("created_at DESC").first(3)
-    # @recent_places << CheckIn.where(:user_id => @user.id, :place_id => @recent.id)
-    
-
-
-    @recent_places = Place.where(:id => CheckIn.where(:user_id => @user.id).order("created_at DESC"))
-    
-    
-    # @feed = Post.order("created_at DESC").first(10)
-    
-    # order("places_id DESC").first(5)
-    # @fav_places = @user.check_ins.place_id.order("created_at DESC").first(10)
-     
-    
-    
-    #<% @check_ins.each do |l|%>
-    #<p><%= l.place_id.name %></p>
-    #<% end %>
-    
+    @fav_places = []
+    user_checkins = CheckIn.where(:user_id => @user.id)
+    user_checkins.each do |place|
+      @fav_places << Place.where(:id => place.id)
+    end
   end
   
   def new
@@ -52,7 +30,6 @@ class UsersController < ApplicationController
   end
   
   def text
-  
     @user = User.find(params[:id])
     @current_user = current_user.fname
   
@@ -63,32 +40,22 @@ class UsersController < ApplicationController
             # :to => +14023219124, #john
             :body => "text from #{@user.fname} #{@user.lname}: #{params[:text]}"
           )
-    
-          #caras number: 4025258220
-          #johns number: 4023219124
-          #kaits number: 4022167901
-          
     redirect_to(user_path(@user.id))
   end
 
   def create
-    
     @user = User.new(params[:user])    
-      
     if @user.save
       session[:user_id] = @user.id  
       redirect_to(edit_user_path(@user.id))
     else
       render "new"
     end
-    
   end
 
   def update
-    
     @user = User.find(params[:id])
     @user.update_attributes(params[:user])
-    
     redirect_to(:groups)
   end
   
