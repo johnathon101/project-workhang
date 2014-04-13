5c9f942345d5cc9353d68cba59d69900135cc2a3
Johnathon <ja.lueth@gmail.com>
2014-04-13 17:47:33 -0500

    remove junk

---
+++ config/initializers/mail.rb
@@ -0,0 +1,15 @@
+require 'development_mail_interceptor'
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port          => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password      => ENV['SENDGRID_PASSWORD'],
+  :domain        => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
+ActionMailer::Base.default_url_options[:host] = ENV['MAIL_HOST_URL'] || "localhost:5000"
+if Rails.env.development?
+  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
+end
