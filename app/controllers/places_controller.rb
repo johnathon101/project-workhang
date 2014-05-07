6f96d5ae2df4d7986e2c91fc6ede7068ff9b9991
Johnathon <ja.lueth@gmail.com>
2014-05-06 22:40:32 -0500

    added json support for hoppyhour requests on place show pages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,38 +2,38 @@ class PlacesController < ApplicationController
   #Create methods to respond to js calls
   respond_to :json, :js
 
-  
+
   @@globalresults=[]
   def new
     @place = Place.new
   end
-  
+
   def new_review
     @place=Place.find_by_id(params[:id]) || Place.find_by_id(params[:place_id])
     @review=Review.find_by_id(params[:review_id]) || Review.new
   end
-  
+
   def new_review_save
     @review=Review.new(params[:review])
     @review.save
     redirect_to("/places/#{@review.place_id}")
   end
-  
+
   def query_google_by_name
     @place = params[:search_term]
     search_query=(@place).downcase.strip.gsub(' ','+')
     #Send search query to google to find the location(Validate Location)
     val_loc=JSON.load(open("https://maps.googleapis.com/maps/api/place/textsearch/json?key=#{ENV["GOOGLE_API_KEY"]}&location=41.2918589,-96.0812485&radius=5000&query=#{search_query}&sensor=false"))
-    
+
     return val_loc
   end
-  
+
   def check_db
     val_loc=@@globalresults
     lat=params[:lat].gsub("e",".").to_f
     lng=params[:lng].gsub("e",".").to_f
     choice=params[:pos].to_i
-   
+
     @check_db=Place.where({:lat=>lat, :lng=>lng}).first
     #If place is in db go to that page
     if @check_db
@@ -49,7 +49,7 @@ class PlacesController < ApplicationController
       @place.lat=lat
       if val_loc["results"][choice]["photos"]!=nil
           @place.photoref=val_loc["results"][choice]["photos"][0]["photo_reference"]
-      else 
+      else
         @place.photoref=""
       end
       @place.save
@@ -58,13 +58,13 @@ class PlacesController < ApplicationController
       @user=current_user
     end
   end
-  
+
 
    def new_manual
      #Google could not appease end user with results
      @place=Place.new
    end
- #  
+ #
    def new_manual_save
      #Get params from user form to add a new place
      @place=Place.new(params[:place])
@@ -77,16 +77,16 @@ class PlacesController < ApplicationController
      longitude=unknown_loc["results"][0]["geometry"]["location"]["lng"]
      #Assign latitude and longitude to place variables
      @place.lat=latitude
-     @place.lng=longitude        
+     @place.lng=longitude
      #Save user created location to DB with geocode from google for lat/lng
      @place.save
   end
-  
+
   def index
-      gon.places=Place.all 
+      gon.places=Place.all
       gon.group_people=[]
   end
-  
+
   def results
     @place = params[:query]
     search_query=(@place).downcase.strip.gsub(' ','+')
@@ -94,10 +94,15 @@ class PlacesController < ApplicationController
     @val_loc=JSON.load(open("https://maps.googleapis.com/maps/api/place/textsearch/json?key=#{ENV["GOOGLE_API_KEY"]}&location=41.2918589,-96.0812485&radius=5000&query=#{search_query}&sensor=false"))
     @@globalresults=@val_loc
   end
-  
+
   def show
-    @present_people=Array.new
     @place = Place.find_by_id(params[:id])
+    lat = (@place.lat).to_s.gsub(".","e")
+    lng = (@place.lng).to_s.gsub(".","e")
+    hh_response = JSON.load(open("http://hoppyhour.herokuapp.com/workhang/#{lat}/#{lng}"))
+    @beers = hh_response["beer"]
+    @foods = hh_response["food"]
+    @present_people=Array.new
     @reviews=Review.where(:place_id => @place.id)
     @imgloc="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{@place.photoref}&sensor=false&key=#{ENV['GOOGLE_API_KEY']}"
     @user = current_user
@@ -108,32 +113,32 @@ class PlacesController < ApplicationController
          checked_in= person_id[:user_id]
          @present_people << User.find_by_id(checked_in)
          i+=1
-       end  
+       end
     else
        @present_people=[]
-    end 
+    end
     @user_checkins=CheckIn.where(:user_id => @user.id, :time_out => nil)
     @check_ins=CheckIn.where(:user_id => @user.id, :place_id => @place.id, :time_out => nil)
     check_admin ? @allow_edit=true : @allow_edit=false
   end
-  
+
   def edit
     @place=Place.find(params[:id])
   end
-  
+
   def update
     binding.pry
     @place=Place.find(params[:id])
     @place.update_attributes(params[:place])
     redirect_to(:root)
   end
-  
+
   def destroy
     @place=Place.find(params[:id])
     @place.destroy
     redirect_to(:root)
   end
-  
+
   #Return correctly formatted string of coordinates for locations in db by passing in id
   def deliver_coordinates(id)
     @created_place=Place.find_by_id(id)
